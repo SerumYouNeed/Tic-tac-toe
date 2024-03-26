8d7ae6d54d385a64af392c6495929076712d029c
Greg Miler <greg.miler@gmail.com>
2024-03-26 17:35:44 +0100

    Game core basic functions

---
+++ view.js
@@ -0,0 +1,16 @@
+export default class View {
+    
+    $ ={};
+
+    constructor() {
+        this.$.menu = document.querySelector('[data-id="menu"]');
+        this.$.menuPopover = document.querySelector('[data-id="menu-popover"]');
+        this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
+        this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
+        this.$.squares = document.querySelectorAll('[data-id="square"]');
+        this.$.modal = document.querySelector('[data-id="modal"]');
+        this.$.modalText = document.querySelector('[data-id="modal-text"]');
+        this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
+        this.$.turn = document.querySelector('[data-id="turn"]');
+    }
+};
