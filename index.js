8af37d175df63dedaaae2555e96fcb3dc1696724
Greg Miler <greg.miler@gmail.com>
2024-03-27 15:32:14 +0100

    View logic: turn indicator & move icon

--- index.js
+++ index.js
@@ -1,143 +1,157 @@
 import View from "./view.js"
 
-const App = {
-    $: {
-        menu: document.querySelector('[data-id="menu"]'),
-        menuPopover: document.querySelector('[data-id="menu-popover"]'),
-        resetBtn: document.querySelector('[data-id="reset-btn"]'),
-        newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
-        squares: document.querySelectorAll('[data-id="square"]'),
-        modal: document.querySelector('[data-id="modal"]'),
-        modalText: document.querySelector('[data-id="modal-text"]'),
-        modalBtn: document.querySelector('[data-id="modal-btn"]'),
-        turn: document.querySelector('[data-id="turn"]'),
-    },
+// const App = {
+//     $: {
+//         menu: document.querySelector('[data-id="menu"]'),
+//         menuPopover: document.querySelector('[data-id="menu-popover"]'),
+//         resetBtn: document.querySelector('[data-id="reset-btn"]'),
+//         newRoundBtn: document.querySelector('[data-id="new-round-btn"]'),
+//         squares: document.querySelectorAll('[data-id="square"]'),
+//         modal: document.querySelector('[data-id="modal"]'),
+//         modalText: document.querySelector('[data-id="modal-text"]'),
+//         modalBtn: document.querySelector('[data-id="modal-btn"]'),
+//         turn: document.querySelector('[data-id="turn"]'),
+//     },
 
-    state: {
-        move: [],
-    },
+//     state: {
+//         move: [],
+//     },
     
-    getGameStatus (move) {
-        const p1Moves = move.filter((moves) => moves.playerId === 1).map((moves) => +moves.squareId);
-        const p2Moves = move.filter((moves) => moves.playerId === 2).map((moves) => +moves.squareId);
+//     getGameStatus (move) {
+//         const p1Moves = move.filter((moves) => moves.playerId === 1).map((moves) => +moves.squareId);
+//         const p2Moves = move.filter((moves) => moves.playerId === 2).map((moves) => +moves.squareId);
         
-        const winPattern = [
-            [1,2,3],
-            [1,5,9],
-            [1,4,7],
-            [2,5,8],
-            [3,5,7],
-            [3,6,9],
-            [4,5,6],
-            [7,8,9],
-        ];
+//         const winPattern = [
+//             [1,2,3],
+//             [1,5,9],
+//             [1,4,7],
+//             [2,5,8],
+//             [3,5,7],
+//             [3,6,9],
+//             [4,5,6],
+//             [7,8,9],
+//         ];
         
-        let winner = null;
+//         let winner = null;
 
-        winPattern.forEach((pattern) => {
-            const p1Wins = pattern.every((v) => p1Moves.includes(v));
-            const p2Wins = pattern.every((v) => p2Moves.includes(v));
-            if(p1Wins) winner = 1;
-            if(p2Wins) winner = 2;
-        });
+//         winPattern.forEach((pattern) => {
+//             const p1Wins = pattern.every((v) => p1Moves.includes(v));
+//             const p2Wins = pattern.every((v) => p2Moves.includes(v));
+//             if(p1Wins) winner = 1;
+//             if(p2Wins) winner = 2;
+//         });
 
 
-        return {
-            status: move.length === 9 || winner != null ? 'complete' : 'in-progress',
-            winner,
-        };
-    },
+//         return {
+//             status: move.length === 9 || winner != null ? 'complete' : 'in-progress',
+//             winner,
+//         };
+//     },
 
-    init() {
-        App.registerEventListeners();
-    },
+//     init() {
+//         App.registerEventListeners();
+//     },
     
-    registerEventListeners() {
-        App.$.menu.addEventListener("click", (event) => {
-            App.$.menuPopover.classList.toggle("hidden");
-        });
+//     registerEventListeners() {
+//         App.$.menu.addEventListener("click", (event) => {
+//             App.$.menuPopover.classList.toggle("hidden");
+//         });
     
-        App.$.resetBtn.addEventListener("click", (event) => {
-            console.log('reset game');
-        });
+//         App.$.resetBtn.addEventListener("click", (event) => {
+//             console.log('reset game');
+//         });
     
-        App.$.newRoundBtn.addEventListener("click", (event) => {
-            console.log('new game');
-        });
+//         App.$.newRoundBtn.addEventListener("click", (event) => {
+//             console.log('new game');
+//         });
 
-        App.$.modalBtn.addEventListener("click", (event) => {
-            App.state.move = [];
-            App.$.squares.forEach(square => square.replaceChildren());
-            App.$.modal.classList.add('hidden');
-        });
+//         App.$.modalBtn.addEventListener("click", (event) => {
+//             App.state.move = [];
+//             App.$.squares.forEach(square => square.replaceChildren());
+//             App.$.modal.classList.add('hidden');
+//         });
     
-        App.$.squares.forEach((square) => {
-            square.addEventListener("click", (event) => {
-                // jeśli już kliknięte okienko to wychodzi
-                const hasMove = (squareId) => {
-                    const existingMove = App.state.move.find(move => move.squareId === squareId);
-                    return existingMove !== undefined; 
-                };
+//         App.$.squares.forEach((square) => {
+//             square.addEventListener("click", (event) => {
+//                 // jeśli już kliknięte okienko to wychodzi
+//                 const hasMove = (squareId) => {
+//                     const existingMove = App.state.move.find(move => move.squareId === squareId);
+//                     return existingMove !== undefined; 
+//                 };
                 
-                // + upewnia że to liczba
-                if(hasMove(+square.id)) {
-                    return;
-                }
+//                 // + upewnia że to liczba
+//                 if(hasMove(+square.id)) {
+//                     return;
+//                 }
 
 
-                // który gracz
-                const lastMove = App.state.move.at(-1);
-                const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
-                const currentPlayer = App.state.move.length === 0 ? 1 : getOppositePlayer(lastMove.playerId);
-                const nextPlayer = getOppositePlayer(currentPlayer);
+//                 // który gracz
+//                 const lastMove = App.state.move.at(-1);
+//                 const getOppositePlayer = (playerId) => (playerId === 1 ? 2 : 1);
+//                 const currentPlayer = App.state.move.length === 0 ? 1 : getOppositePlayer(lastMove.playerId);
+//                 const nextPlayer = getOppositePlayer(currentPlayer);
 
-                const squareIcon = document.createElement('i');  
-                const turnIcon = document.createElement('i'); 
-                const turnLabel = document.createElement('p');              
-                turnLabel.innerText = `Player ${nextPlayer}, your turn!`;
+//                 const squareIcon = document.createElement('i');  
+//                 const turnIcon = document.createElement('i'); 
+//                 const turnLabel = document.createElement('p');              
+//                 turnLabel.innerText = `Player ${nextPlayer}, your turn!`;
 
-                if(currentPlayer === 1) {
-                    squareIcon.classList.add('fa-solid', 'fa-x', 'yellow');
-                    turnIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
-                    turnLabel.classList = 'turquoise';
-                } else {
-                    squareIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
-                    turnIcon.classList.add('fa-solid', 'fa-x', 'yellow');
-                    turnLabel.classList = 'yellow';
-                }
+//                 if(currentPlayer === 1) {
+//                     squareIcon.classList.add('fa-solid', 'fa-x', 'yellow');
+//                     turnIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
+//                     turnLabel.classList = 'turquoise';
+//                 } else {
+//                     squareIcon.classList.add('fa-solid', 'fa-o', 'turquoise');
+//                     turnIcon.classList.add('fa-solid', 'fa-x', 'yellow');
+//                     turnLabel.classList = 'yellow';
+//                 }
 
-                App.$.turn.replaceChildren(turnIcon, turnLabel);
+//                 App.$.turn.replaceChildren(turnIcon, turnLabel);
 
-                // aktualizuje stan
-                App.state.move.push({
-                    squareId: +square.id,
-                    playerId: currentPlayer,  
-                })
+//                 // aktualizuje stan
+//                 App.state.move.push({
+//                     squareId: +square.id,
+//                     playerId: currentPlayer,  
+//                 })
 
-                square.replaceChildren(squareIcon);
+//                 square.replaceChildren(squareIcon);
                 
-                const game = App.getGameStatus(App.state.move);
+//                 const game = App.getGameStatus(App.state.move);
 
-                // gdy gra się zakończy 
-                let msg = '';
-                if(game.status === 'complete') {
-                    App.$.modal.classList.remove('hidden');
-                    if(game.winner) {
-                        msg = `Player ${game.winner} wins!`;
-                    } else {
-                        msg = `Tie!`;
-                    }
-                    App.$.modalText.textContent = msg;
-                }
-            });
-        });
-    },
-};
+//                 // gdy gra się zakończy 
+//                 let msg = '';
+//                 if(game.status === 'complete') {
+//                     App.$.modal.classList.remove('hidden');
+//                     if(game.winner) {
+//                         msg = `Player ${game.winner} wins!`;
+//                     } else {
+//                         msg = `Tie!`;
+//                     }
+//                     App.$.modalText.textContent = msg;
+//                 }
+//             });
+//         });
+//     },
+// };
 
-window.addEventListener("load", App.init);
+// window.addEventListener("load", App.init);
 
 function init() {
     const view = new View();
+
+    view.bindGameResetEvent(event => {
+        
+    })
+
+    view.bindNewRoundEvent(event => {
+
+    })
+
+    view.bindPlayerMoveEvent(event => {
+        view.setTurnIndicator(2);
+        view.handlePlayerMove(event.target, 1);
+    })
+
     console.log(view.$.turn);
 };
 
--- view.js
+++ view.js
@@ -1,16 +1,88 @@
 export default class View {
     
     $ ={};
+    $$ ={};
 
     constructor() {
-        this.$.menu = document.querySelector('[data-id="menu"]');
-        this.$.menuPopover = document.querySelector('[data-id="menu-popover"]');
-        this.$.resetBtn = document.querySelector('[data-id="reset-btn"]');
-        this.$.newRoundBtn = document.querySelector('[data-id="new-round-btn"]');
-        this.$.squares = document.querySelectorAll('[data-id="square"]');
-        this.$.modal = document.querySelector('[data-id="modal"]');
-        this.$.modalText = document.querySelector('[data-id="modal-text"]');
-        this.$.modalBtn = document.querySelector('[data-id="modal-btn"]');
-        this.$.turn = document.querySelector('[data-id="turn"]');
+        this.$.menu = this.#qs('[data-id="menu"]');
+        this.$.menuBtn = this.#qs('[data-id="menu-btn"]');
+        this.$.menuPopover = this.#qs('[data-id="menu-popover"]');
+        this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
+        this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
+        this.$.modal = this.#qs('[data-id="modal"]');
+        this.$.modalText = this.#qs('[data-id="modal-text"]');
+        this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
+        this.$.turn = this.#qs('[data-id="turn"]');
+        
+        this.$$.squares = this.#qsAll('[data-id="square"]');
+        
+        // eventy tylko interfejsu, bez wpływu na stan
+        this.$.menuBtn.addEventListener('click', event => {
+            this.#toggleMenu();  
+        });
     }
-};
+
+    // rejestracja wszystkich eventów
+
+    bindGameResetEvent(handler) {
+        this.$.resetBtn.addEventListener('click', handler);
+    }
+
+    bindNewRoundEvent(handler) {
+        this.$.newRoundBtn.addEventListener('click', handler);
+    }
+
+    bindPlayerMoveEvent(handler) {
+        this.$$.squares.forEach(square => {
+            square.addEventListener('click', handler);
+        });
+    }
+
+    // metody DOM
+    // # trzyma to jako prywatne. nie ma do nich dostępu z zewnątrz.
+    #toggleMenu() {
+        this.$.menuPopover.classList.toggle('hidden');
+        this.$.menuBtn.classList.toggle('border');
+
+        const icon = this.$.menuBtn.querySelector('i');
+
+        icon.classList.toggle("fa-chevron-up");
+        icon.classList.toggle("fa-chevron-down");
+    };
+
+    handlePlayerMove(squareEl, player) {
+        const icon = document.createElement('i');
+        icon.classList.add('fa-solid', player === 1 ? 'fa-x' : 'fa-o', player === 1 ? 'yellow' : 'turquoise');
+        squareEl.replaceChildren(icon);
+    }
+
+    setTurnIndicator(player) {
+        const icon = document.createElement('i');
+        const label = document.createElement('p');
+
+        this.$.turn.classList.add(player === 1 ? 'yellow' : 'turquoise');        
+        this.$.turn.classList.remove(player === 1 ? 'turquoise' : 'yellow');        
+
+        icon.classList.add('fa-solid', player === 1 ? 'fa-x' : 'fa-o');
+
+        label.innerText = player === 1 ? 'Player 1, your turn!' : 'Player 2, your turn!';
+
+        this.$.turn.replaceChildren(icon, label);
+    };
+
+    #qs(selector, parent) {
+        const el = parent ? parent.querySelector(selector) : document.querySelector(selector);
+
+        if(!el) throw new Error('Could not find element');
+
+        return el;
+    }
+
+    #qsAll(selector) {
+        const elList = document.querySelectorAll(selector);
+
+        if(!elList) throw new Error('Could not find element');
+
+        return elList;
+    }
+}; 
